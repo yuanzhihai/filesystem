ef61ba0d3d5813bee382a5a6d1e988e77ba9a5a6
yuanzhihai <396751927@qq.com>
2022-09-26 13:12:06 +0800

    更新 league/flysystem 3.0

--- src/Adapter/ObsAdapter.php
+++ src/Adapter/ObsAdapter.php
@@ -1,15 +1,29 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types = 1 );
 
 namespace yzh52521\Filesystem\Adapter;
 
+use Obs\ObsClient;
 use yzh52521\Filesystem\Contract\AdapterInterface;
 
 class ObsAdapter implements AdapterInterface
 {
     public function createAdapter(array $options)
     {
-        return new \yzh52521\Flysystem\Obs\ObsAdapter($options);
+        $config            = [
+            'key'      => $options['key'],
+            'secret'   => $options['secret'],
+            'bucket'   => $options['bucket'],
+            'endpoint' => $options['endpoint'],
+        ];
+        $client            = new ObsClient( $config );
+        $config['options'] = [
+            'url'             => '',
+            'endpoint'        => $options['endpoint'],
+            'bucket_endpoint' => '',
+            'temporary_url'   => '',
+        ];
+        return new \yzh52521\Flysystem\Obs\ObsAdapter( $client,$options['bucket'],$options['prefix'],null,null,$config['options'] );
     }
 }
